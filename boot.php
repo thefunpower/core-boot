9b98d8603b87aa7ef203ba7111f360a1f56d0113
sunkang <13127955213@qq.com>
2023-02-21 13:53:02 +0800

    Update boot.php

--- boot.php
+++ boot.php
@@ -5,9 +5,7 @@
     LICENSE: https://github.com/thefunpower/core/blob/main/LICENSE.md 
     Connect Email: sunkangchina@163.com 
     Code Vesion: v1.0.x
-*/
-
-date_default_timezone_set('PRC');
+*/ 
 //版本号
 if(!defined('DEBUG')) define('VERSION', "1.0.1");
 //线上可以关闭PHP错误提示
@@ -16,23 +14,20 @@ if(!defined('ADMIN_DIR_NAME'))define("ADMIN_DIR_NAME", 'admin-dev');
 if(!defined('ADMIN_COOKIE_NAME'))define("ADMIN_COOKIE_NAME", 'user_id'); 
 //定义一些路径常量一般不用修改
 if(!defined('PATH')) define('PATH', realpath(__DIR__ . '/../../../').'/');
-if(!defined('THE_FUN_PATH')) define('THE_FUN_PATH', PATH . 'vendor/thefunpower/');  
-if(!defined('SYS_PATH')) define('SYS_PATH', THE_FUN_PATH . 'core/');   
+if(!defined('THE_FUN_PATH')) define('THE_FUN_PATH', realpath(PATH . 'vendor/thefunpower').'/');  
+if(!defined('SYS_PATH')) define('SYS_PATH', THE_FUN_PATH . 'core/');  
 /**
  * 错误提示 
  */
 if (DEBUG) {
   ini_set('display_errors', 'on');
+  //error_log() 函数，记录具体错误
   error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_WARNING);
+  ini_set('error_log', PATH . 'data/phplog.log');
 } else {
   ini_set('display_errors', 'off');
   error_reporting(0); 
 }
-/**
- * error_log() 函数，记录具体错误
- */
-ini_set('log_errors', 'On');
-ini_set('error_log', PATH . 'data/phplog.log');
 /**
 * 数据库配置
 */
@@ -43,12 +38,14 @@ include PATH.'config.ini.php';
 $medoo_db_config = $config;
 if(!file_exists('db_active_main')){
   include THE_FUN_PATH.'db_medoo/inc/db/boot.php';
-} 
+}  
 /**
 * 加载autoload
 */
 global $autoload;
-$autoload = include PATH . 'vendor/autoload.php';
+if(!$autoload){
+  $autoload = include PATH.'vendor/autoload.php';  
+}
 $autoload->addPsr4('app\\',PATH.'app/');
 $autoload->addPsr4('service\\',PATH.'service/'); 
 $autoload->addPsr4('helper\\',PATH.'helper/'); 
@@ -65,6 +62,7 @@ location / {
  */
 global $router;
 $router = new \Bramus\Router\Router(); 
+//加载常用功能
 include SYS_PATH . '/boot.php'; 
 //开始
 do_action('init');
@@ -72,11 +70,9 @@ $input  = g();
 /**
 * 加载dump目录下的*.helper.php
 */
-$dir = PATH . 'dump'; 
-echo $dir;
+$dir = PATH . 'dump';  
 if(is_dir($dir)){
-  $all = glob($dir.'/*.helper.php');
-  pr($all);exit;
+  $all = glob($dir.'/*.helper.php'); 
   foreach($all as $v){
     include $v.'.php';
   }
