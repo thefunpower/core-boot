00ccadceb1565db80dabdbb4d1e6be51e0330e8f
sunkang <13127955213@qq.com>
2023-11-16 14:20:23 +0800

    new version

--- boot.php
+++ boot.php
@@ -1,11 +1,4 @@
 <?php
-/*
-    Copyright (c) 2021-2031, All rights reserved.
-    This is NOT a freeware
-    LICENSE: https://github.com/thefunpower/core/blob/main/LICENSE.md 
-    Connect Email: sunkangchina@163.com 
-    Code Vesion: v1.0.x
-*/ 
 //版本号
 if(!defined('DEBUG')) define('VERSION', "1.0.1");
 //线上可以关闭PHP错误提示
